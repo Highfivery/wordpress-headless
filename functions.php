52cf29beec51d9ca9a485954e6cd8d44f771cd49
Ben Marshall <ben.s.marshall@icloud.com>
2023-02-03 08:10:43 -0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Headless functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Headless
+ * @since Headless 1.0.0
+ */
+
